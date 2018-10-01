120e6b4a624b1d10d561aae116eb1342068abe0d
Stephanie <smedina7@miners.utep.edu>
2018-09-30 21:29:44 -0600

    Adding fixes to Server code

--- file-transfer-lab/fileServer.py
+++ file-transfer-lab/fileServer.py
@@ -29,14 +29,26 @@ while True:
     sock, addr = lsock.accept()
 
     from framedSock import framedSend, framedReceive
+    
+    #create receive file if it doesnt exist
+    if not os.path.exists("receivedFile.txt"):
+        open("receivedFile.txt","w+")
+        
+    f = open("receivedFile.txt","a")  #keep adding to the file; append until it stops receiving
 
     if not os.fork():
         print("new child process handling connection from", addr)
         while True:
             payload = framedReceive(sock, debug)
+            
+            #start writing that data to the file
+            f.write(payload)
+            
             if debug: print("rec'd: ", payload)
             if not payload:
                 if debug: print("child exiting")
                 sys.exit(0)
             payload += b"!"             # make emphatic!
             framedSend(sock, payload, debug)
+            
+    f.close() #close file
