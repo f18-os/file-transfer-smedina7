22efb09ad46b022a48e93db1e5da4f15d78dc77f
Stephanie <smedina7@miners.utep.edu>
2018-09-30 20:57:59 -0600

    Adding test file and playing with code

--- file-transfer-lab/fileClient.py
+++ file-transfer-lab/fileClient.py
@@ -1,2 +1,75 @@
-import sys
+#! /usr/bin/env python3
+
+# Echo client program
+import socket, sys, re
+
+sys.path.append("../lib")       # for params
+import params
+
+from framedSock import framedSend, framedReceive
+
+
+switchesVarDefaults = (
+    (('-s', '--server'), 'server', "127.0.0.1:50001"),
+    (('-d', '--debug'), "debug", False), # boolean (set if present)
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+
+progname = "framedClient"
+paramMap = params.parseParams(switchesVarDefaults)
+
+server, usage, debug  = paramMap["server"], paramMap["usage"], paramMap["debug"]
+
+if usage:
+    params.usage()
+
+
+try:
+    serverHost, serverPort = re.split(":", server)
+    serverPort = int(serverPort)
+except:
+    print("Can't parse server:port from '%s'" % server)
+    sys.exit(1)
+
+s = None
+for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
+    af, socktype, proto, canonname, sa = res
+    try:
+        print("creating sock: af=%d, type=%d, proto=%d" % (af, socktype, proto))
+        s = socket.socket(af, socktype, proto)
+    except socket.error as msg:
+        print(" error: %s" % msg)
+        s = None
+        continue
+    try:
+        print(" attempting to connect to %s" % repr(sa))
+        s.connect(sa)
+    except socket.error as msg:
+        print(" error: %s" % msg)
+        s.close()
+        s = None
+        continue
+    break
+
+if s is None:
+    print('could not open socket')
+    sys.exit(1)
+
+#open file to send it to server
+
+#STORE the name of the file that user wants to send
+clientFile = sys.args[1]
+
+#check if file exists
+if not os.path.exists(clientFile):
+    print ("File %s doesn't exist! Exiting" % outputFname)
+    exit()
+
+# attempt to open file to start sending to server
+with open(clientFile, 'r') as rFile:
+    for line in rFile:
+        #1 indicates the current position 
+        sendBytes = rFile.seek(1,100)  #get the contents from the current position until 100 bytes for limit
+        framedSend(s, sendBytes, debug)  #send
 
