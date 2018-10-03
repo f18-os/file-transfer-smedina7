dc113c3694335cf8caeb6c41a9540175bdae44f1
Stephanie <smedina7@miners.utep.edu>
2018-10-03 15:13:27 -0600

    More Cleaning and fixing param path

--- file-transfer-lab/Server/fileServer.py
+++ file-transfer-lab/Server/fileServer.py
@@ -2,7 +2,7 @@
 
 import sys
 
-sys.path.append("../lib")       # for params
+sys.path.append("../../lib")    # for params
 
 import os, socket, params
 
@@ -57,8 +57,12 @@ while True:
                 #save name of file to create/copy file: and later append
                 if not os.path.exists(payload):
                     open(payload,"a+")
+                else:
+                    print("File %s already exists! Exiting...", payload.decode())
+                    sys.exit(0)
                 #open file once done
-                f = open(payload,"ab")
+            
+            f = open(payload,"ab")
                 
             #check if get has been called
 ##            if b"get:" in payload:
